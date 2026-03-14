58decb3408d2b83d92d2a01a44b39d693f9e4d12
Grant King <grantryan.king@gmail.com>
2026-03-14 15:06:05 -0700

    Update Chapter030.vue
    Add a special formatting section for Dorothy's letter.
    
    Also format the whitespace and indentation of the .vue file

--- vue-project/src/components/chapters/Chapter030.vue
+++ vue-project/src/components/chapters/Chapter030.vue
@@ -8,20 +8,31 @@
     </div>
     <div class="h-16"></div>
     <div>
-The writer of these Oz stories has received a little note from Princess Dorothy of Oz which, for a time, has made him feel rather disconcerted. The note was written on a broad, white feather from a stork's wing, and it said:
-</div>
-<div>
-YOU WILL NEVER HEAR ANYTHING MORE ABOUT OZ, BECAUSE WE ARE NOW CUT OFF FOREVER FROM ALL THE REST OF THE WORLD. BUT TOTO AND I WILL ALWAYS LOVE YOU AND ALL THE OTHER CHILDREN WHO LOVE US.
-</div>
-<div>
-DOROTHY GALE.
-</div>
-<div>
-This seemed to me too bad, at first, for Oz is a very interesting fairyland. Still, we have no right to feel grieved, for we have had enough of the history of the Land of Oz to fill six story books, and from its quaint people and their strange adventures we have been able to learn many useful and amusing things.
-</div>
-<div>
-So good luck to little Dorothy and her companions. May they live long in their invisible country and be very happy!
-</div>
+        The writer of these Oz stories has received a little note from Princess Dorothy of Oz which, for a time, has
+        made him feel rather disconcerted. The note was written on a broad, white feather from a stork's wing, and it
+        said:
+    </div>
+    <div class="bg-emerald-100/20 p-8 rounded font-black font-serif tracking-wide border-1 border-black/20">
+        <div>
+            You will never hear anything more about OZ, because we are now cut off forever from all the rest of the world.
+            But Toto and I will always love you and all the other children who love us.
+        </div>
+        <div class="h-8"></div>
+        <div>
+            <span class="bg-linear-to-t from-emerald-900 to-emerald-200 bg-clip-text text-black/20 text-shadow-xs text-shadow-emerald-200">
+                Dorothy Gale
+            </span>
+        </div>
+    </div>
+    <div>
+        This seemed to me too bad, at first, for Oz is a very interesting fairyland. Still, we have no right to feel
+        grieved, for we have had enough of the history of the Land of Oz to fill six story books, and from its quaint
+        people and their strange adventures we have been able to learn many useful and amusing things.
+    </div>
+    <div>
+        So good luck to little Dorothy and her companions. May they live long in their invisible country and be very
+        happy!
+    </div>
 
 </template>
 
